4d50760a86fcd5609daae211e768516c54a5e4b2
Julien Seng <julien.seng@insee.fr>
2018-05-31 14:39:12 +0200

    update readme

--- src/Scanner.jsx
+++ src/Scanner.jsx
@@ -14,8 +14,8 @@ export default class Scanner extends React.Component {
           name: "Live",
           type: "LiveStream",
           constraints: {
-            width: 640,
-            height: 480,
+            width: 320,
+            height: 240,
             facingMode: "environment" // or user
           }
         },
